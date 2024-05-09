b65f1ef33b22b894f818ee70acae6a4690834a2a
bayufadayan <bayufadayan@gmail.com>
2024-05-09 23:40:44 +0700

    fix validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// JS Banner Slider
 var slideIndex = 1;
 showDivs(slideIndex); // Menampilkan slide pertama saat memuat
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
@@ -21,7 +22,57 @@ function showDivs(n) {
   imgList[slideIndex - 1].style.display = "block";
 }
 
-
 setInterval(() => {
   plusDivs(1);
 }, 3000);
+
+// JS Validation
+// const nameInput = document.getElementById("client-name");
+// document.getElementById("submit").addEventListener("click", function () {
+//     const name = nameInput.value;
+//     if (name = "") {
+//         document.getElementById('client-name-error').innerText = "Isi boss, jangan di kosongin"
+//     }
+// })
+
+function validateForm() {
+  const name = document.forms["contact-form"]["client-name"].value;
+  const email = document.forms["contact-form"]["email"].value;
+  const interest = document.forms["contact-form"]["user-interest"].value;
+  let isValid = true;
+  let errorMessage =
+    "<i class='fa-solid fa-circle-exclamation'></i> Harap isi dengan benar.";
+
+  if (name === "") {
+    document.getElementById("client-name-error").innerHTML = errorMessage;
+    isValid = false;
+  } else {
+    document.getElementById("client-name-error").innerHTML = "";
+  }
+
+  if (email === "" || !validateEmail(email)) {
+    document.getElementById("email-error").innerHTML = errorMessage;
+    isValid = false;
+  } else {
+    document.getElementById("email-error").innerHTML = "";
+  }
+
+  if (interest === "") {
+    document.getElementById("user-interest-error").innerHTML =
+      "<i class='fa-solid fa-circle-exclamation'></i> Pilih yang bener";
+    isValid = false;
+  } else {
+    document.getElementById("user-interest-error").innerHTML = "";
+  }
+
+  if (isValid) {
+    alert("Hi " + name + ", Formulir berhasil dikirim!");
+  }
+
+  return isValid;
+}
+
+function validateEmail(email) {
+  const re = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+  return re.test(email);
+}
